89c008ef1d75035100f681038bdf4cd6ad3dc9a1
dasingh92 <dewanarunsingh@outlook.com>
2026-03-05 10:22:19 +1100

    Improves the performance of the API.

--- FdjCodeChallenge.Api/Controllers/CustomerControllerEndpoints.cs
+++ FdjCodeChallenge.Api/Controllers/CustomerControllerEndpoints.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Text.Json;
 using FdjCodeChallenge.Api.Database;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FdjCodeChallenge.Api.Controllers;
@@ -8,6 +9,7 @@ namespace FdjCodeChallenge.Api.Controllers;
 public static class CustomerControllerEndpoints
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web);
+    private static Dictionary<long, CustomerDetailsResponse> _customerDetailsCache = new();
 
     public static void MapCustomerControllerEndpoints(this WebApplication app)
     {
@@ -29,18 +31,11 @@ public static class CustomerControllerEndpoints
             return Results.NotFound();
         }
         // In production this would be done in a separate service class, but for the sake of this challenge we can do it here in the controller.
-        var customerNameEndpoint = configuration.GetValue<string>("CustomerDetailsApiEndpoint") ?? throw new NullReferenceException("CustomerDetailsApiEndpoint configuration value is missing");
-        var candidateId = configuration.GetValue<string>("CandidateId") ?? throw new NullReferenceException("CandidateId configuration value is missing");
-        httpClient.BaseAddress = new Uri(customerNameEndpoint);
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"customer?customerId={customerId}&candidateId={candidateId}");
-        var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-        if(!httpResponseMessage.IsSuccessStatusCode)
+        var customerDetails = await GetCustomerDetails(customerId, httpClient, configuration, logger);
+        if (customerDetails is null)
         {
             return Results.NotFound();
         }
-        var customerDetails = JsonSerializer.Deserialize<CustomerDetailsResponse>(await httpResponseMessage.Content.ReadAsStringAsync(), _jsonSerializerOptions);
-        if (customerDetails is null)        
-            return Results.NotFound();
         var customerName = customerDetails.CustomerName;
         var totalStandToWin = database.PlacedBets
             .Where(bet => bet.CustomerId == customerId)
@@ -48,12 +43,32 @@ public static class CustomerControllerEndpoints
 #pragma warning disable CA1873 // Avoid potentially expensive logging
         logger.LogInformation("Retrieved stats for customer {CustomerId} in {ElapsedMilliseconds} ms", customerId, stopwatch.ElapsedMilliseconds);
 #pragma warning restore CA1873 // Avoid potentially expensive logging
-        return httpResponseMessage.IsSuccessStatusCode
-            ? Results.Ok(new CustomerStatsResponse(customerId, customerName, totalStandToWin))
-            : Results.NotFound();
+        return Results.Ok(new CustomerStatsResponse(customerId, customerName, totalStandToWin, stopwatch.ElapsedMilliseconds));
     }
 
-    public record CustomerStatsResponse(long CustomerId, string Name, decimal TotalStandToWin);
+    private static async Task<CustomerDetailsResponse?> GetCustomerDetails(long customerId, HttpClient httpClient, IConfiguration configuration, ILogger logger)
+    {
+        if (_customerDetailsCache.TryGetValue(customerId, out var cachedCustomerDetails))
+        {
+            return cachedCustomerDetails;
+        }
+        var customerNameEndpoint = configuration.GetValue<string>("CustomerDetailsApiEndpoint") ?? throw new NullReferenceException("CustomerDetailsApiEndpoint configuration value is missing");
+        var candidateId = configuration.GetValue<string>("CandidateId") ?? throw new NullReferenceException("CandidateId configuration value is missing");
+        httpClient.BaseAddress = new Uri(customerNameEndpoint);
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"customer?customerId={customerId}&candidateId={candidateId}");
+        var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            return null;
+        }
+        var customerDetails = JsonSerializer.Deserialize<CustomerDetailsResponse>(await httpResponseMessage.Content.ReadAsStringAsync(), _jsonSerializerOptions);
+        if (customerDetails is null)
+            return null;
+        _customerDetailsCache.TryAdd(customerId, customerDetails);
+        return customerDetails;
+    }
+
+    public record CustomerStatsResponse(long CustomerId, string Name, decimal TotalStandToWin, long millisecondsToRetrieve);
 }
 
 internal record CustomerDetailsResponse
